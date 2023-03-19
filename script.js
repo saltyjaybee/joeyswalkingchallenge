f5a613ca6f16996d00bf0010ae26377edd3686f2
saltyjaybee <128197356+saltyjaybee@users.noreply.github.com>
2023-03-19 18:19:21 +1000

    Update script.js

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ function initMap() {
     (response, status) => {
       if (status === 'OK') {
         directionsRenderer.setDirections(response);
-        updateMarkerPosition(); // Moved this line here
+        updateMarkerPosition();
       } else {
         console.error(`Directions request failed due to ${status}`);
       }
@@ -81,93 +81,7 @@ function initMap() {
     );
   }
 
-  setInterval(updateMarkerPosition, 60000); // Update the position every minute
-}
-
-google.maps.event.addDomListener(window, 'load', initMap);
-';
-
-function initMap() {
-  const map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
-    center: { lat: -27.464145, lng: 141.278832 },
-  });
-
-  const startMarker = new google.maps.Marker({
-    position: { lat: -19.2535, lng: 146.81724 },
-    map: map,
-    title: "St Joseph's Catholic School the Strand",
-  });
-
-  const joeysMarker = new google.maps.Marker({
-    position: { lat: -19.2535, lng: 146.81724 },
-    map: map,
-    title: 'Joeys 150th Walkers',
-    icon: {
-      url: 'https://maps.google.com/mapfiles/ms/icons/blue-dot.png',
-    },
-  });
-
-  const directionsService = new google.maps.DirectionsService();
-  const directionsRenderer = new google.maps.DirectionsRenderer({
-    map: map,
-    preserveViewport: true,
-  });
-
-  directionsRenderer.setOptions({
-    suppressMarkers: true,
-    polylineOptions: { strokeColor: 'red' },
-  });
-
-  const start = new google.maps.LatLng(-19.2535, 146.81724);
-  const end = new google.maps.LatLng(-37.37982, 140.83947);
-
-  directionsService.route(
-    {
-      origin: start,
-      destination: end,
-      travelMode: 'WALKING',
-    },
-    (response, status) => {
-      if (status === 'OK') {
-        directionsRenderer.setDirections(response);
-        updateMarkerPosition(); // Moved this line here
-      } else {
-        console.error(`Directions request failed due to ${status}`);
-      }
-    }
-  );
-
-  function updateMarkerPosition() {
-    $.getJSON(
-      `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/Form%20Responses%201!A2:B?key=${apiKey}`,
-      (data) => {
-        const totalKms = data.values
-          .map((row) => parseFloat(row[1]))
-          .reduce((sum, kms) => sum + kms, 0);
-
-        const route = directionsRenderer.getDirections();
-        const path = route.routes[0].overview_path;
-        let remainingKms = totalKms;
-
-        for (let i = 0; i < path.length - 1; i++) {
-          const a = path[i];
-          const b = path[i + 1];
-          const distance = google.maps.geometry.spherical.computeDistanceBetween(a, b) / 1000;
-          if (remainingKms >= distance) {
-            remainingKms -= distance;
-          } else {
-            const fraction = remainingKms / distance;
-            const interpolatedLatLng = google.maps.geometry.spherical.interpolate(a, b, fraction);
-            joeysMarker.setPosition(interpolatedLatLng);
-            break;
-          }
-        }
-      }
-    );
-  }
-
-  setInterval(updateMarkerPosition, 60000); // Update the position every minute
+  setInterval(updateMarkerPosition, 60000);
 }
 
 google.maps.event.addDomListener(window, 'load', initMap);
