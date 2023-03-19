27e13ca1b072c52b8fb7fe0601053157078f540e
saltyjaybee <128197356+saltyjaybee@users.noreply.github.com>
2023-03-19 21:10:01 +1000

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const apiKey = 'AIzaSyD5SCv6wFw-XXdK13L369BPmnTA_59fxRg';
 
 function initMap() {
   const map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 6,
+    zoom: 5,
     center: { lat: -26.35118, lng: 144.11533 },
   });
 
